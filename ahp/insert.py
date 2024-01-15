05905e5d9dcd3a893caf6876c7f8759839c674fb
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-01-15 20:28:52 +0100

    ranking creation from config file

--- ahp/insert.py
+++ ahp/insert.py
@@ -76,6 +76,71 @@ def insert_alternative_ranking(a: list[int], criterion: int, expert_id: int, m: 
             conn.commit()
 
 
+def create_ranking(decoded_json):
+    """
+    given decoded json dictionary (a python dict) with ranking info,
+    insert them into the database
+    """
+
+    crit_ids = {}
+    for c in decoded_json["criteria"]:
+        query = """
+        insert into criteria (name, description, parent_criterion)
+        values (?, ?, ?)
+        """
+
+        p_str = c["parent_criterion"]
+        parent = crit_ids[p_str] if p_str != "" else None
+
+        info = (c["name"], c["description"], parent)
+        cur.execute(query, info)
+        conn.commit()
+
+        crit_ids[c["name"]] = cur.lastrowid
+
+
+    for a in decoded_json["alternatives"]:
+        query = """
+        insert into alternatives (name, description)
+        values (?, ?)
+        """
+
+        info = (a["name"], a["description"])
+        cur.execute(query, info)
+        conn.commit()
+
+        alt_id = cur.lastrowid
+
+        for (crit, desc) in a["criteria_descriptions"].items():
+            if desc == "": continue
+
+            crit_id = crit_ids[crit]
+            query = """
+            insert into AlternativeCriteriaDesc (alternative, criterion, description)
+            values (?, ?, ?)
+            """
+
+            info = (alt_id, crit_id, desc)
+            cur.execute(query, info)
+            conn.commit()
+
+
+    for s in decoded_json["scales"]:
+        query = "insert into scales (value, description) values (?, ?)"
+
+        info = (s["value"], s["description"])
+        cur.execute(query, info)
+        conn.commit()
+
+
+    for e in decoded_json["experts"]:
+        query = "insert into experts (name, email) values (?, ?)"
+
+        info = (e["name"], e["email"])
+        cur.execute(query, info)
+        conn.commit()
+
+
 if __name__ == "__main__":
     criteria = get_criteria_ids_and_names()
     alternatives = get_alternative_ids_and_names()
