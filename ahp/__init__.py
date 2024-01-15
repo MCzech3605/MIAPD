ae752c020d230a204804c7ea24e89598e2b3c448
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-01-15 19:39:36 +0100

    ranking calculation with subcriteria

--- ahp/__init__.py
+++ ahp/__init__.py
@@ -1,5 +1,6 @@
 import sqlite3
 from pprint import pprint
+from itertools import groupby
 import numpy as np
 
 conn: sqlite3.Connection = sqlite3.connect("baza.db")
@@ -20,18 +21,19 @@ def eigenvalue_method(matrix):
 
     return priority_vector
 
-# wip for multiple layers of criteria
+
 def calculate_steps(tuples):
     relationships = {t[0]: t[1] for t in tuples}
 
     def steps_to_ancestor(node):
         steps = 0
-        while node is not None:
+        while node != -1:
             steps += 1
             node = relationships.get(node)
         return steps - 1  
 
-    result = {t[0]: steps_to_ancestor(t[0]) for t in tuples}
+    result = {t[0]: steps_to_ancestor(t[0]) + 1 for t in tuples}
+    result[-1] = 0
     return result
 
 
@@ -62,8 +64,21 @@ def get_alternatives_with_desc(cur=cur):
     return res
 
 
+def get_criteria():
+    query = "select id, parent_criterion from criteria order by id"
+    cur.execute(query)
+
+    return cur.fetchall()
+
+
 def get_bottom_criteria(cur=cur):
-    query = "select * from criteria where parent_criterion is null order by id"
+    query = f"""
+        select * from criteria
+        where id not in (
+            select parent_criterion from criteria
+            where parent_criterion is not null
+        )
+        """
     cur.execute(query)
 
     return cur.fetchall()
@@ -82,20 +97,24 @@ def get_experts(cur=cur):
 
 
 def summarize_priorities(m):
+    m = np.array(m, dtype=object)
     return np.prod(m, axis=0) ** (1. / len(m))
 
 
-def create_ranking(alternatives, criteria, expert_ids, cur=cur):
-    n_crit = len(criteria)
+def create_ranking(alternatives, bottom_criteria, expert_ids, cur=cur):
+    n_crit = len(bottom_criteria)
     n_alt = len(alternatives)
     n_exp = len(expert_ids)
     m = [[[[1 for _ in range(n_alt)] for _ in range(n_alt)] for _ in range(n_crit)] for _ in range(n_exp)]
 
-    criteria_index = {criteria[i][0]: i for i in range(n_crit)}
+    bottom_criteria = [(c[0], c[3] if c[3] is not None else -1) for c in bottom_criteria]
+
+    bottom_criteria_index = {bottom_criteria[i][0]: i for i in range(n_crit)}
+    alternative_index = {alternatives[i][0]: i for i in range(n_alt)}
     expert_index = {expert_ids[i]: i for i in range(n_exp)}
     
     for expert_id in expert_ids:
-        for crit in criteria:
+        for crit in bottom_criteria:
             crit_id = crit[0]
             query = f"""
                 select first_alternative, second_alternative, value
@@ -107,36 +126,83 @@ def create_ranking(alternatives, criteria, expert_ids, cur=cur):
             comparisons = cur.fetchall()
             
             for first, second, val in comparisons:
-                m[expert_index[expert_id]][criteria_index[crit_id]][criteria_index[first]][criteria_index[second]] = val
+                m[expert_index[expert_id]][bottom_criteria_index[crit_id]][alternative_index[first]][alternative_index[second]] = val
 
     alt_wages_per_expert = [[eigenvalue_method(np.array(a)) for a in exp_m] for exp_m in m]
     alternative_wages = summarize_priorities(alt_wages_per_expert)
 
     m = [[[1 for _ in range(n_crit)] for _ in range(n_crit)] for _ in range(n_exp)]
 
+    criteria = [(c[0], c[1] if c[1] is not None else -1) for c in get_criteria()]
+    criteria.sort(key=lambda x: x[1])
+    steps = calculate_steps(criteria)
+    parents = {c[0]: c[1] for c in criteria}
+    parents[-1] = None
+
+    children_graph = [(id, [c[0] for c in group]) for id, group in groupby(criteria, key=lambda x: x[1])]
+    children_graph.sort(key=lambda x: steps[x[0]], reverse=True)
+    n_g = len(children_graph)
+
+    levels = [(id, [c[0] for c in group]) for id, group in groupby(steps.items(), key=lambda x: x[1])]
+    levels.sort(key=lambda x: x[0], reverse=True)
+
+    parent_index = {children_graph[i][0]: i for i in range(n_g)}
+    child_index = [{g[1][i]: i for i in range(len(g[1]))} for g in children_graph]
+
+    m = []
+
     for expert_id in expert_ids:
+        ma = [[[1 for _ in g[1]] for _ in g[1]] for g in children_graph]
+
         query = f"""
-            select first_criterion, second_criterion, value
+            select first_criterion, second_criterion, parent_criterion, value
             from CriteriaComparisons 
             join scales on scale=scales.id
+            join Criteria on first_criterion=Criteria.id
             where expert={expert_id}
             """
         cur.execute(query)
         comparisons = cur.fetchall()
 
-        for first, second, val in comparisons:
-            m[expert_index[expert_id]][criteria_index[first]][criteria_index[second]] = val
+        for first, second, p, val in comparisons:
+            if p is None: p = -1
+            p_i = parent_index[p]
+            ma[p_i][child_index[p_i][first]][child_index[p_i][second]] = val
 
-    crit_wages_per_expert = [eigenvalue_method(np.array(a)) for a in m]
+        m.append(ma)
+
+
+    # only from pairwise comparisons
+    crit_wages_per_expert = [[eigenvalue_method(np.array(a)) for a in exp_m] for exp_m in m]
     criterion_wages = summarize_priorities(crit_wages_per_expert)
+
+
+    wage_prod = [1.0 for _ in range(n_crit)]
+    for c in bottom_criteria:
+        prod = 1.0
+        crit_id = c[0]
+        parent_id = parents[crit_id]
+
+        while crit_id != -1:
+            i = parent_index[parent_id]
+            cmp = criterion_wages[i]
+            val = cmp[child_index[i][crit_id]]
+            prod *= val
+
+            crit_id = parent_id
+            parent_id = parents[crit_id]
+        
+        wage_prod[bottom_criteria_index[c[0]]] = prod
+
 
     res = [0 for _ in range(n_alt)]
     for i in range(n_alt):
         sum = 0
         for j in range(n_crit):
-            sum += criterion_wages[j] * alternative_wages[j][i]
+            sum += wage_prod[j] * alternative_wages[j][i]
             
         res[i] = sum
+
 
     return res
 
@@ -152,7 +218,7 @@ if __name__ == "__main__":
     res = create_ranking(alternatives, bottom_criteria, experts)
     pprint(res)
 
-    pprint(get_alternatives_with_desc())
+    # pprint(get_alternatives_with_desc())
     
     cur.close()
     conn.close()
